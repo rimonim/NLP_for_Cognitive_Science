5f9833e2694e12846e1cadcd6ecb8768d000a5bb
Louis Teitelbaum <louis@teitelbaum.us>
2022-03-23 21:55:40 +0200

    Update corpus.R

--- MineCraft/R/corpus.R
+++ MineCraft/R/corpus.R
@@ -36,21 +36,6 @@ readminecraftconvdf <- function(x) {
   convo <- convo[grepl(pattern = "Mission has started", convo$text) == FALSE, ]
 }
 
-  # FUNCTION: if one person takes more than one turn in talk, this collapses them into one
-  # takes a convdf (output of readconvdf FUN above)
-collapseturns <- function(convdf) {
-  trashrows <- c()
-  for(row in 2:nrow(convdf)){
-    if(convdf[row, "participant"] == convdf[row -1, "participant"] & 
-       convdf[row, "conversation"] == convdf[row -1, "conversation"]){
-      convdf[row, "text"] <- paste(convdf[row - 1, "text"], convdf[row, "text"])
-      trashrows <- append(trashrows, row - 1)
-    }
-  }
-  convdf <- convdf[!(1:nrow(convdf) %in% trashrows), ]
-  convdf
-}
-
   # make a big dataframe with all the conversations
 filenames <- list.files("/Users/louisteitelbaum/R.Projects/MinecraftCorpus/MinecraftCorpusDialogues")
 minecraftcorpusdf <- lapply(filenames, readminecraftconvdf)
@@ -62,7 +47,7 @@ library(quanteda.textstats)
 library(hunspell)
 
 # make a quanteda corpus, then a document-feature matrix
-minecraftcorpus <- quanteda::corpus(collapseturns(minecraftcorpusdf), text_field = "text")
+minecraftcorpus <- quanteda::corpus(minecraftcorpusdf, text_field = "text")
 minecraftcorpus_tokens <- quanteda::tokens(minecraftcorpus, remove_punct = TRUE)
 minecraftcorpus_dfm <- quanteda::dfm(minecraftcorpus_tokens)
 
@@ -203,151 +188,3 @@ assignInNamespace("textstat_proxy", my_textstat_proxy, ns = "quanteda.textstats"
 # TF-IDF vectorization
 minecraftcorpus_tfidf <- quanteda::dfm_tfidf(minecraftcorpus_dfm)
 minecraftcorpus_tfidf_mat <- as.matrix(minecraftcorpus_tfidf)
-minecraftcorpus_tfidf_mat[minecraftcorpus_tfidf_mat == 0] <- NA
-
-# cosine similarity
-tfidfsim <- function(dfmsource, x) {
-  tfidfsim <- quanteda::dfm_subset(dfmsource, conversation == x)
-  tfidfsim <- quanteda.textstats::textstat_simil(tfidfsim, margin = "documents", method = "cosine")
-  tfidfsim <- as.matrix(tfidfsim)
-}
-utterance.sim.simmax <- function(dfmsource, x) {
-  # subset conversation from tfidf document-feature matrix and compute similarity matrix
-  tfidfsim <- tfidfsim(dfmsource, minecraftcorpusdf$conversation[minecraftcorpusdf$file == x][1])
-  
-  # average effort for each of g's utterances
-  lines <- 1:nrow(tfidfsim)
-  sims <- c()
-  for(line in lines) {
-    prevutsims <- tfidfsim[line, 1:(line-1)]
-    simmax <- max(prevutsims)
-      # weight <- 1/(1 + length(prevutsims) - which.max(prevutsims))
-    sims <- append(sims, simmax)
-  }
-  sims
-}
-addsim <- function(dfmsource, x) {
-  thisconvo <- collapseturns(readminecraftconvdf(x))
-  thisconvo <- cbind(thisconvo, data.frame(similarity = utterance.sim.simmax(dfmsource, x)))
-  thisconvo[1, "similarity"] <- NA
-  thisconvo
-}
-filenames <- list.files("/Users/louisteitelbaum/R.Projects/MinecraftCorpus/MinecraftCorpusDialogues")
-minecraftcorpusdf <- lapply(X = filenames, FUN = addsim, dfmsource = minecraftcorpus_tfidf)
-minecraftcorpusdf <- do.call(rbind, minecraftcorpusdf)
-
-# orthographic length
-minecraftcorpusdf$length <- nchar(minecraftcorpusdf$text)
-
-# repair
-minecraftcorpusdf$repair <- grepl("\\?", minecraftcorpusdf$text) & minecraftcorpusdf$role == "B"
-
-# tfidfmean
-minecraftcorpusdf$tfidfmean <- rowMeans(as.matrix(minecraftcorpus_tfidf_mat), na.rm = TRUE)
-
-# tfidfsum
-minecraftcorpusdf$tfidfsum <- rowSums(as.matrix(minecraftcorpus_tfidf_mat), na.rm = TRUE)
-
-  # aggregate data
-library(tidyverse)
-    # this one is useful for the vec_rep_each function
-library(vctrs)
-
-d <- minecraftcorpusdf %>%
-  group_by(conversation, role, participant) %>%
-  summarise(
-    avglength = mean(length, na.rm = T),
-    avgtfidfmean = mean(tfidfmean, na.rm = T),
-    avgsim = mean(similarity, na.rm = T),
-    n = n()
-    ) %>%
-  pivot_wider(names_from = role, values_from = c(participant, avglength, avgtfidfmean, avgsim, n)) %>%
-  group_by(conversation) %>%
-  summarise(
-    participant_b = max(participant_B, na.rm = T),
-    participant_a = max(participant_A, na.rm = T),
-    avglength_b = max(avglength_B, na.rm = T),
-    avglength_a = max(avglength_A, na.rm = T),
-    avgtfidfmean_b = max(avgtfidfmean_B, na.rm = T),
-    avgtfidfmean_a = max(avgtfidfmean_A, na.rm = T),
-    avgsim_b = max(avgsim_B, na.rm = T),
-    avgsim_a = max(avgsim_A, na.rm = T),
-    n_b = max(n_B, na.rm = T),
-    n_a = max(n_A, na.rm = T)
-  )
-
-d <- d %>%
-  group_by(participant_b) %>%
-  mutate(
-    blen = mean(avglength_b),
-    btfidf = mean(avgtfidfmean_b),
-    bsim = mean(avgsim_b)
-  ) %>%
-  group_by(participant_a) %>%
-  mutate(
-    alen = mean(avglength_a),
-    atfidf = mean(avgtfidfmean_a),
-    asim = mean(avgsim_a)
-  ) %>%
-  ungroup(participant_a)
-
-totalrepairs <- minecraftcorpusdf %>%
-  group_by(conversation) %>%
-  summarise(
-    repairs = sum(repair)
-  )
-
-d$repairfreq <- totalrepairs$repairs / d$n_a
-
-    # full participant aggregates
-agglengthsb <- d %>%
-  group_by(participant_b) %>%
-  summarise(
-    blen = mean(blen),
-    btfidf = mean(btfidf),
-    bsim = mean(bsim),
-    participant = participant_b
-  ) %>%
-  arrange(participant_b)
-
-agglengthsa <- d %>%
-  group_by(participant_a) %>%
-  summarise(
-    alen = mean(alen),
-    atfidf = mean(btfidf),
-    asim = mean(asim),
-    participant = participant_a
-  ) %>%
-  arrange(participant_a)
-
-aggs <- merge(x = agglengthsb,
-                    y = agglengthsa,
-                    by = "participant",
-                    all.x = TRUE)
-
-aggs$agglen <- (aggs$blen + aggs$alen) / 2
-aggs$aggtfidf <- (aggs$btfidf + aggs$atfidf) / 2
-aggs$aggsim <- (aggs$bsim + aggs$asim) / 2
-
-aggs <- aggs %>%
-  group_by(participant) %>%
-  summarise(
-    agglen = mean(agglen),
-    aggtfidf = mean(aggtfidf),
-    aggsim = mean(aggsim)
-  )
-
-d <- d %>%
-  arrange(participant_b)
-pnumber <- as.vector(rle(d$participant_b)[1])
-agglengths <- vec_rep_each(aggs$agglen, pnumber[["lengths"]])
-aggtfidfs <- vec_rep_each(aggs$aggtfidf, pnumber[["lengths"]])
-aggsims <- vec_rep_each(aggs$aggsim, pnumber[["lengths"]])
-
-d <- d %>%
-  arrange(participant_b) %>%
-  mutate(
-    agglen = agglengths,
-    aggtfidf = aggtfidfs,
-    aggsim = aggsims
-  )
